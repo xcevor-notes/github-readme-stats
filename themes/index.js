6efa182982bc861d873463cbb54c728dad4553bb
xcevor-notes <127529898+xcevor-notes@users.noreply.github.com>
2023-09-07 09:59:39 +0900

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -3,7 +3,7 @@ export const themes = {
     title_color: "2f80ed",
     icon_color: "4c71f2",
     text_color: "434d58",
-    bg_color: "fffefe",
+    bg_color: "0d1117",
     border_color: "e4e2e2",
   },
   default_repocard: {
